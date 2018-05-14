c0bfa8c8feb2e83f53ec07741f41ea4d158c41e2
Josh Peak <neozenith.dev@gmail.com>
2018-05-14 14:44:20 +1000

    Plotting in iterm2

---
+++ python/README.md
@@ -0,0 +1,9 @@
+# Python Data Vis
+
+```
+brew install python3
+pip3 install -r requirements.txt
+export MPLBACKEND="module://itermplot"
+export ITERMPLOT=rv
+python3 test.py
+```
---
+++ python/test.py
@@ -0,0 +1,14 @@
+#! /usr/local/bin/python3
+import numpy as np
+import pandas as pd
+import scipy as sp
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./data/MA5830-ASS2-Dataset.csv', index_col=0)
+
+print(df.describe())
+fig, axes = plt.subplots(nrows=2, ncols=1)
+
+df.plot(ax=axes[0], kind='box')
+df.plot(ax=axes[1], kind='kde')
+plt.show()
